12740a7501e892dcbb5de70d55ad4e2bb117cb87
Amarendra <116885674+Amarendrapratihari@users.noreply.github.com>
2022-10-28 17:02:33 +0530

    Update Employee.java

--- springboot-crud-web-app/src/main/java/com/springboot/crudapp/model/Employee.java
+++ springboot-crud-web-app/src/main/java/com/springboot/crudapp/model/Employee.java
@@ -5,10 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
 import javax.persistence.Table;
-
-
 
 @Entity
 @Table(name= "employee")
