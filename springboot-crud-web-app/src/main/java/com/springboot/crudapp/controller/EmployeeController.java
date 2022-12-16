8fa635d2947a732a0b93a9bd3648d187f3d4f81c
Amarendra Pratihari <116885674+Amarendrapratihari@users.noreply.github.com>
2022-12-16 17:18:05 +0530

    Update EmployeeController.java

--- springboot-crud-web-app/src/main/java/com/springboot/crudapp/controller/EmployeeController.java
+++ springboot-crud-web-app/src/main/java/com/springboot/crudapp/controller/EmployeeController.java
@@ -1,4 +1,5 @@
 package com.springboot.crudapp.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.springboot.crudapp.model.Employee;
 import com.springboot.crudapp.service.EmployeeService;
 
@@ -30,7 +30,6 @@ public class EmployeeController {
 		Employee employee = new Employee();
 		model.addAttribute("employee", employee);
 		return "new_employee";
-
 	}
 
 	
